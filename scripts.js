d2ea09cf2d2b8665a7bf39372be744ed2abce39b
Thapelo <thapeloletsapa@gmail.com>
2024-05-24 10:54:04 +0200

    "Added console log statements for spacing and formatting, and reformatted code for readability."

--- scripts.js
+++ scripts.js
@@ -26,35 +26,39 @@ names.forEach(name => console.log(name));
 // console log name along with matching province
 console.log('---NAME (PROVINCE)---')
 names.forEach((name, index) => console.log(`${name} (${provinces[index]})`));
-
+console.log(" ");
 //PART 2
 // using map to create a new array of province names in all uppercase
 // console log new array
 console.log("---UPPERCASE PROVINCES---");
+
 const upperCaseProvinces = provinces.map((province) => province.toUpperCase());
 console.log(upperCaseProvinces);
-
+console.log(" ");
 // PART 3
 // Creating a new array using map that contains the length of each name.
 console.log("-----Lengths for each Name------")
+
 const nameLengths = names.map(name => `${name} (${name.length})`);
 console.log(nameLengths);
 
-
+console.log(" ");
 //PART 4 
 //Sorting: Use sort to alphabetically sort the provinces.
 //SORT() This method mutates the array and returns a reference to the same array
 console.log("-----Provinces in alphabetical order-----")
+
 const sortedProvinces = provinces.sort();
 console.log(sortedProvinces);
-
+console.log(" ");
 // PART 5
 // Filtering Cape: Use filter to remove provinces containing "Cape". Log the count of remaining provinces.
 console.log("----Provinces without CAPE----")
+
 const provincesWithoutCape = provinces.filter( province =>!province.toLowerCase().includes("cape"));
 console.log(`Remaining provinces: ${provincesWithoutCape.length}`);
 console.log(provincesWithoutCape);
-
+console.log(" ");
 //PART 6
 //Finding 'S': Create a boolean array using map and some to determine if a name contains the letter 'S'.
 console.log("-----Boolean of whether or not name contains S-----");
@@ -65,7 +69,7 @@ console.log(namesContainingLetterS);
 console.log("Returns one boolean Value after using some() on the return")
 const namesContainsLetterS = namesContainingLetterS.some(Boolean);
 console.log(namesContainsLetterS);
-
+console.log(" ");
 //PART 7 
 // Use reduce to transform the names array into an object mapping names to their respective provinces.
 const nameAndProvinceObject = names.reduce((object,name,index) =>{
@@ -76,11 +80,13 @@ console.log(nameAndProvinceObject);
 
 //--------------------------------------------Advanced Exercises--------------------------------------------
 
-
+console.log(" ");
 //PART 1
 // Iterate over the products array, logging each product name.
 console.log("-----Product Names-----");
 products.map((productName) => console.log(productName.product));
+
+console.log(" ");
 
 
 //PART 2
@@ -88,6 +94,7 @@ products.map((productName) => console.log(productName.product));
 // FILTER() Returns the elements of an array that meet the condition specified in a callback function
 console.log("-----Products with names shorter than or equal to 5 characters-----");
 console.log(products.filter((product) => product.product.length <= 5));
+console.log(" ");
 
 
 //PART 3
@@ -98,20 +105,17 @@ console.log("-----Total price-----");
 console.log((products.filter((product) => 
  !(product.price === '' || product.price ===" "))).reduce((sum, product) => 
     sum + parseInt(product.price), 0));
+console.log(" ");
 
 
-//PART 3
-// Filter out products without prices, convert string prices to numbers, and calculate the total price using reduce
-// REDUCE() The return value of the callback function is the accumulated result, 
-// and is provided as an argument in the next call to the callback function. 
-console.log("-----Total price-----");
-console.log((products.filter((product) => 
- !(product.price === '' || product.price ===" "))).reduce((sum, product) => 
-    sum + parseInt(product.price), 0));
+
 
 //  PART 4
 //  Use reduce to concatenate all product names into a single string.
 console.log("----String with NO white spaces in between---");
+
 console.log(products.reduce((concatNames, product) => concatNames + product.product,''));
+console.log(" ");
 console.log("----String with white spaces in between---");
+
 console.log(products.reduce((concatNames, product) => concatNames + product.product+' ',''));
