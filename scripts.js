c317f7b0db97a99d984da90db559f5986a7fb066
Thapelo <thapeloletsapa@gmail.com>
2024-05-24 10:28:03 +0200

    "Added three parts of code: logging provinces and names, logging names with matching provinces, and creating new arrays using map for uppercase provinces and name lengths."

--- scripts.js
+++ scripts.js
@@ -13,3 +13,29 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ]
+
+
+//PART 1
+// using forEach to log each name and each province to the console
+console.log("-----PROVINCES-----");
+provinces.forEach(province => console.log(province));
+console.log("-------NAMES-------");
+names.forEach(name => console.log(name));
+
+
+// console log name along with matching province
+console.log('---NAME (PROVINCE)---')
+names.forEach((name, index) => console.log(`${name} (${provinces[index]})`));
+
+//PART 2
+// using map to create a new array of province names in all uppercase
+// console log new array
+console.log("---UPPERCASE PROVINCES---");
+const upperCaseProvinces = provinces.map((province) => province.toUpperCase());
+console.log(upperCaseProvinces);
+
+// PART 3
+// Creating a new array using map that contains the length of each name.
+console.log("-----Lengths for each Name------")
+const nameLengths = names.map(name => `${name} (${name.length})`);
+console.log(nameLengths);
